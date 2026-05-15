87c550373a99d5269a037d9f33120d38d6cb6902
ruanfelixdev <ruanfeliz7777@gmail.com>
2026-05-15 13:59:00 -0300

    Create Soma de uma linha.py

---
+++ matriz/Soma de uma linha.py
@@ -0,0 +1,33 @@
+'''
+Crie um algoritmo que:
+Leia uma matriz 3x3
+Mostre a soma dos elementos da
+segunda linha
+'''
+
+matriz = []
+lista1 = []
+lista2 = [] 
+lista3 = []
+for i in range(2):
+    n1 = int(input('digite um numero:'))
+    lista1.append(n1)
+
+matriz.append(lista1)
+    
+for x in range(2):
+    n2 = int(input('digite um numero:'))
+    lista2.append(n2)
+
+matriz.append(lista2)
+
+for j in range(2):
+    n3 = int(input('digite um numero:'))
+    lista3.append(n3)
+
+matriz.append(lista3)
+
+print(f'matriz\n{matriz}')
+
+soma = sum(matriz[1])
+print(f'soma da segunda coluna {soma}')

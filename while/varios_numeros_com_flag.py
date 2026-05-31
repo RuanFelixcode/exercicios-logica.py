ffdbac4d8775a3fa858508ebefb60c672e54240d
ruanfelixdev <ruanfeliz7777@gmail.com>
2026-05-31 13:43:34 -0300

    Create varios_numeros_com_flag.py

---
+++ while/varios_numeros_com_flag.py
@@ -0,0 +1,9 @@
+'''Exercício Python 066: Crie um programa que leia números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre elas (desconsiderando o flag).'''
+soma = 0
+while True:
+    numero = int(input('digite um numero pra parar (999):').strip())
+    if numero == 999:
+        break
+    soma += numero
+    
+print(f'soma dos valores {soma}')
